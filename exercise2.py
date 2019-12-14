a09bc4a385c29b7ce650df0999c4135472634dfa
thekatt0 <contact@katelynkuhn.com>
2019-12-14 15:17:20 -0600

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -1,6 +1,9 @@
 num = input('Enter a number. ')
 
-if (int(num) % 2) == 0:
+if (int(num) % 4) == 0:
+    print(str(num) + ' is divisible by 4.')
+
+elif (int(num) % 2) == 0:
     print(str(num) + ' is an even number.')
 
 else:
