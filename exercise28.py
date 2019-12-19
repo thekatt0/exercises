bf61ca90b293d225820815ba36427dc8543f4453
thekatt0 <contact@katelynkuhn.com>
2019-12-19 07:00:32 -0600

    Practice Python Exercise 28

---
+++ exercise28.py
@@ -0,0 +1,19 @@
+#inputs
+numA = input('Insert the first number. ')
+numB = input('Insert the second number. ')
+numC = input('Insert the last number. ')
+
+#convert to int
+int(numA)
+int(numB)
+int(numC)
+
+#comparisons not using max
+if numA > numB and numA > n1umC:
+        print(str(numA) + ' is the largest')
+
+elif numB > numA and numB > numC:
+        print(str(numB) + ' is the largest')
+
+else:
+    print(str(numC) + ' is the largest')
