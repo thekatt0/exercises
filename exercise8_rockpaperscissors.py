17fb164734dbb540376eac2e0fad32af28cb3c42
thekatt0 <contact@katelynkuhn.com>
2019-12-24 06:52:16 -0600

    Update exercise8_rockpaperscissors.py
    fixed

--- exercise8_rockpaperscissors.py
+++ exercise8_rockpaperscissors.py
@@ -5,20 +5,17 @@ p2 = input('Player 2; Rock, Paper or Scissors? ')
 
 
 
-if ((p1.upper() == 'PAPER' and p2.upper() == 'ROCK') or (p1.upper() == 'SCISSORS' and p2.upper() == 'PAPER') or (p1.upper() == 'ROCK' and p2.upper() == 'SCISSORS')):
-    print('Player 2 wins! ')
+if((p1.upper() == 'PAPER' and p2.upper() == 'ROCK') or (p1.upper() == 'SCISSORS' and p2.upper() == 'PAPER') or (p1.upper() == 'ROCK' and p2.upper() == 'SCISSORS')):
+    print('Player 1 wins! ')
 
     #TIE
 elif ((p1.upper() == 'ROCK' and p2.upper() == 'ROCK') or (p1.upper() == 'PAPER' and p2.upper() == 'PAPER') or (p1.upper() == 'SCISSORS' and p2.upper() == 'SCISSORS')):
-
+    print('Its a tie ')
 
     #PLAYER 1 WINS
-elif p1.upper() == 'PAPER' and p2.upper() == 'ROCK':
-        print('Player 1 wins! ')
-elif p1.upper() == 'SCISSORS' and p2.upper() == 'PAPER':
-        print('Player 1 wins! ')
-elif p1.upper() == 'ROCK' and p2.upper() == 'SCISSORS':    
-        print('Player 1 wins! ')
+
+elif((p2.upper() == 'PAPER' and p1.upper() == 'ROCK') or (p2.upper() == 'SCISSORS' and p1.upper() == 'PAPER') or (p2.upper() == 'ROCK' and p1.upper() == 'SCISSORS')):
+    print('Player 2 wins! ')
 
 
 
