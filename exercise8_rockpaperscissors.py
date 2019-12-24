91ecc3b44e1a95c552e38bb9203152765d7c2aaa
thekatt0 <contact@katelynkuhn.com>
2019-12-24 06:53:58 -0600

    Update exercise8_rockpaperscissors.py
    notes

--- exercise8_rockpaperscissors.py
+++ exercise8_rockpaperscissors.py
@@ -1,19 +1,19 @@
+
 p1 = input('Player 1; Rock, Paper or Scissors? ')
 p2 = input('Player 2; Rock, Paper or Scissors? ')
 
-#compare
 
 
-
+#comparisons
+#player 1 wins
 if((p1.upper() == 'PAPER' and p2.upper() == 'ROCK') or (p1.upper() == 'SCISSORS' and p2.upper() == 'PAPER') or (p1.upper() == 'ROCK' and p2.upper() == 'SCISSORS')):
     print('Player 1 wins! ')
 
-    #TIE
+#tied
 elif ((p1.upper() == 'ROCK' and p2.upper() == 'ROCK') or (p1.upper() == 'PAPER' and p2.upper() == 'PAPER') or (p1.upper() == 'SCISSORS' and p2.upper() == 'SCISSORS')):
     print('Its a tie ')
 
-    #PLAYER 1 WINS
-
+#player 2 wins
 elif((p2.upper() == 'PAPER' and p1.upper() == 'ROCK') or (p2.upper() == 'SCISSORS' and p1.upper() == 'PAPER') or (p2.upper() == 'ROCK' and p1.upper() == 'SCISSORS')):
     print('Player 2 wins! ')
 
