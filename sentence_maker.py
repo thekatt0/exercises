de95c50fdc58acb6adf1a456729acb6018dd5ece
thekatt0 <contact@katelynkuhn.com>
2020-01-03 05:14:10 -0600

    Create sentence_maker.py

---
+++ sentence_maker.py
@@ -0,0 +1,21 @@
+say = ''
+said = []
+
+def sentence_maker(phrase):
+    interrogatives = ("how", "when", "why", "where")
+    capitalized = phrase.capitalize()
+    if phrase.startswith(interrogatives):
+        return "{}?".format(capitalized)
+    else:
+        return "{}.".format(capitalized)    
+
+
+results = []
+while True:
+    user_input = input("Say something: ")
+    if user_input == "\end":
+        break
+    else:
+        results.append(sentence_maker(user_input))
+
+print(" ".join(results))
